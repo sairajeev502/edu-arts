5164d5a933fa7bf052f09cafc44a2a01f0492376
vemurisujin <vemurisujinkumar2508@gmail.com>
2021-10-23 21:23:03 +0530

    (L) Changes in index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const express=require('express');
-const ejs=require('ejs');
+const express=require('express
 const app=express();
+const path=require('path');
+
 app.use(express.static("public"));
 app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.use(express.static(path.join(__dirname,"/public")));
+
 app.get("/",function(req,res){
   res.render("index");
 })
-
-
-
-
 
 app.listen(3000,function(){
   console.log("Server is listening at port 3000");
