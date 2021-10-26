280200f340ab11331c67de49e3f9f02d56d17e92
Dhanush Adithya <s.dhanushadithya@gmail.com>
2021-10-26 22:06:48 +0530

    Added packages and methods

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express=require('express');
-const app=express();
-const path=require('path');
+const express = require('express');
+const mongoose = require('mongoose')
+
+const app = express();
+
+const authRoutes = require('./routes/authRoutes')
+
+require('dotenv').config()
 
 app.use(express.static("public"));
+app.use(express.json())
 app.set("view engine","ejs");
 
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    app.listen(8000, () => {
+        console.log("http://localhost:8000/")
+    })
+})
 
-app.get("/",(req,res)=>{
-  res.render("index");
-});
-app.get("/signup",function(req,res){
-  res.render("signup");
-});
-app.get("/login",function(req,res){
-  res.render("login");
-});
-app.get("/",function(req,res){
-  res.render("index");
-});
+app.get("/", (req, res) => res.render("index"));
+app.get("/signup", (req, res) => res.render("signup"));
+app.get("/login",(req, res) => res.render("login"));
 
-app.listen(8000,function(){
-  console.log("Server is listening on port 8000");
-});
+app.use(authRoutes)
